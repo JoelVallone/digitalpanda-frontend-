f9a2dd8267d6cea081550d0bdf44a89d570045fb
Joël Vallone <joel.vallone@gmail.com>
2018-12-23 17:24:35 +0100

    Filter location selection

--- src/app/sensor/history/history.sensor.component.ts
+++ src/app/sensor/history/history.sensor.component.ts
@@ -13,20 +13,34 @@ export class HistorySensorComponent  implements OnInit {
 
   measureKeys: Observable<Array<SensorMeasureMetaData>>;
 
-  @ViewChild(NgbAccordion) accordion: NgbAccordion;
+  isSelectionCollapsed: Boolean;
+  sensorSelectionText: String = '';
+  displayData: Boolean;
+
+  // @ViewChild(NgbAccordion) accordion: NgbAccordion;
 
   constructor(public sensorService: SensorService) {
+
+    // TODO: Persist selection into a service
+    this.isSelectionCollapsed = false;
+    this.displayData = false;
+    this.sensorSelectionText = 'Select sensors';
   }
 
   ngOnInit() {
       this.measureKeys = this.sensorService.loadMeasurekeys();
-      console.log(this.accordion);
   }
 
+  toggleSensorSelection() {
+    this.isSelectionCollapsed = !this.isSelectionCollapsed;
+    this.displayData = this.isSelectionCollapsed;
+  }
 
   onMeasureKeysSelected(selectedMeasureKeys: Array<SensorMeasureMetaData>) {
-    // TODO: Further handle selection measure selection
-    this.accordion.expand('history-debug');
+    // TODO: Further handle measure selection
+    this.isSelectionCollapsed = true;
+    this.displayData = true;
+    this.sensorSelectionText = 'Change sensor selection';
     console.log('Selected measure keys: ' + JSON.stringify(selectedMeasureKeys));
   }
 }
--- src/app/sensor/selector/sensor-identification/sensor-identification.component.ts
+++ src/app/sensor/selector/sensor-identification/sensor-identification.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit, OnChanges, SimpleChanges, Output, EventEmitte
 import { FormGroup, FormArray, FormControl, FormBuilder } from '@angular/forms';
 
 import { SensorMeasureMetaData, SensorMeasureTypeDetails, SensorMeasureType } from './../../sensor.classes';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-sensor-identification',
@@ -13,68 +14,120 @@ export class SensorIdentificationComponent implements OnInit, OnChanges {
   @Input() measureKeys: Array<SensorMeasureMetaData>;
   @Output() selectedMeasureKeys = new EventEmitter<Array<SensorMeasureMetaData>>();
 
-  public form;
+  private measureTypesByLocationMap = new Map<string, Array<SensorMeasureMetaData>>();
 
-  get locationsMeasures(): FormArray {
-    return this.form.get('locationsMeasures') as FormArray;
-  }
+  public form: FormGroup;
+
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
     this.form  = this.fb.group({
+      locations : this.fb.array([]),
       locationsMeasures: this.fb.array([])
+    });
+
+    this.locations.valueChanges.subscribe(locationsVal => {
+      console.log('change in location' + JSON.stringify(locationsVal));
+      this.setLocationMeasures(
+        this.getMeasureTypesByLocation(
+          locationsVal.filter(location => location.isSelected)
+                    .map( location => location.location)));
     });
   }
 
+  private getMeasureTypesByLocation(locations: Array<string> ): Map<string, Array<SensorMeasureMetaData>> {
+    const selectedMeasuresByLocation = new Map<string, Array<SensorMeasureMetaData>>();
+    locations.forEach(location =>
+      selectedMeasuresByLocation.set(location, this.measureTypesByLocationMap.get(location)));
+    return selectedMeasuresByLocation;
+  }
+
   ngOnChanges(changes: SimpleChanges): void {
-    this.updateSelectableKeys(changes.measureKeys.currentValue);
+    if (changes.measureKeys.currentValue != null) {
+      this.updateSelectableKeys(changes.measureKeys.currentValue);
+    }
+  }
+
+  toggleAllLocations(): void {
+    if (this.isMeasureTypesByLocationEmtpy()) {
+      this.setLocationMeasures(this.measureTypesByLocationMap);
+    } else {
+      this.setLocationMeasures(new Map());
+    }
+  }
+
+  isMeasureTypesByLocationEmtpy(): Boolean {
+    return this.locationsMeasures.length === 0;
+  }
+
+  get locations(): FormArray {
+    return this.form.get('locations') as FormArray;
+  }
+
+  get locationsMeasures(): FormArray {
+    return this.form.get('locationsMeasures') as FormArray;
   }
 
   private updateSelectableKeys(newMeasureKeys: Array<SensorMeasureMetaData>) {
-    if (newMeasureKeys != null) {
+    this.measureTypesByLocationMap = new Map<string, Array<SensorMeasureMetaData>>();
+    newMeasureKeys.forEach(measureKey => {
+      if (this.measureTypesByLocationMap.has(measureKey.location)) {
+        this.measureTypesByLocationMap.get(measureKey.location).push(measureKey);
+      } else {
+        this.measureTypesByLocationMap.set(measureKey.location, [measureKey]);
+      }
+    });
 
-      const newMeasureTypesByLocationMap = new Map<string, Array<SensorMeasureMetaData>>();
-      newMeasureKeys.forEach(measureKey => {
-        if (newMeasureTypesByLocationMap.has(measureKey.location)) {
-          newMeasureTypesByLocationMap.get(measureKey.location).push(measureKey);
-        } else {
-          newMeasureTypesByLocationMap.set(measureKey.location, [measureKey]);
-        }
-      });
+    this.setLocations(this.measureTypesByLocationMap);
+  }
 
-      this.locationsMeasures.reset();
-      newMeasureTypesByLocationMap.forEach(
-        (measureTypes: SensorMeasureMetaData[], location: string, map: Map<string, SensorMeasureMetaData[]>) => {
+  private setLocations(measureTypesByLocationMap: Map<string, Array<SensorMeasureMetaData>>): void {
+    this.locations.reset();
+    this.measureTypesByLocationMap.forEach(
+      (measureTypes: SensorMeasureMetaData[], location: string) => {
+        const locationGroup = this.fb.group({
+          location,
+          isSelected: this.fb.control(false)
+        });
+        this.locations.push(locationGroup);
+      }
+    );
+  }
 
-          const locationMeasures: FormGroup =  this.fb.group({
-            location,
-            measures : this.fb.array([])
-          });
+  private setLocationMeasures(measureTypesByLocationMap: Map<string, Array<SensorMeasureMetaData>>): void {
+    this.locationsMeasures.controls.splice(0);
+    measureTypesByLocationMap.forEach(
+      (measureTypes: SensorMeasureMetaData[], location: string) => {
+        console.log('location ' + location);
+        const locationMeasures: FormGroup =  this.fb.group({
+          location,
+          measures : this.fb.array([])
+        });
 
-          measureTypes.forEach((measureKey) => {
-            (locationMeasures.get('measures') as FormArray).push(
-              this.fb.group({
-                measureKey,
-                measureTypeName : SensorMeasureMetaData.getTypeDetail(measureKey.type).typeName,
-                isSelected: this.fb.control(false)
-              }));
-          });
+        measureTypes.forEach((measureKey) => {
+          (locationMeasures.get('measures') as FormArray).push(
+            this.fb.group({
+              measureKey,
+              measureTypeName : SensorMeasureMetaData.getTypeDetail(measureKey.type).typeName,
+              isSelected: this.fb.control(false)
+            }));
+        });
 
-          this.locationsMeasures.push(locationMeasures);
-      });
-    }
+        this.locationsMeasures.push(locationMeasures);
+    });
   }
 
   canSubmit(): Boolean {
     return this.locationsMeasures.controls.some(locationMeasures =>
       (locationMeasures.get('measures') as FormArray).controls.some(measure =>
         measure.get('isSelected').value)
-      );
+      ) && this.locationsMeasures.length !== 0;
   }
 
   onSubmit() {
-    if (!this.form.valid) {
+    console.log('submit');
+    if (!this.form.valid || !this.canSubmit()) {
       console.error('Invalid form' + JSON.stringify(this.form.errors));
       return;
     }
